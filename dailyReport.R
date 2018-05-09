75934ee65c17c037db2414e5f6777de8837aa3b3
reachdk <netmaildeepak@gmail.com>
2018-05-09 21:00:45 +0530

    Initial commit

--- dailyReport.R
+++ dailyReport.R
@@ -2,4 +2,5 @@
 # Objective : TODO
 # Created by: deepak
 # Created on: 9/5/18
-
+dailyCount = read.csv('Numbers - dailyCount.csv', header = TRUE)
+dailyCount[,2:11] <- lapply(dailyCount[,2:11],function(dailyCount){as.numeric(gsub(",", "", dailyCount))})
